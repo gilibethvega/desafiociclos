154b5826e12d8e10a856b05dd72d4b66cf72a3c6
Gilibeth Vega <gvega@creditu.com>
2020-10-26 08:59:43 -0300

    se completa desafío z

---
+++ letraz.rb
@@ -0,0 +1,22 @@
+n = ARGV[0].to_i
+
+n.times do |i|
+  print "*"
+end
+  print "\n"
+
+(n-2).times do |i|
+  n.times do |j|
+    if (j+1) == ((n-i).pred)
+      print '*'
+    else
+      print " "
+    end
+  end
+  print "\n"
+end
+
+n.times do |i|
+  print "*"
+end
+print "\n"
